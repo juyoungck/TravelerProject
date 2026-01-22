091288b7d9b5bd47cec0ae87f107e1faa2670be4
juyoungck <juyoungck@gmail.com>
2026-01-22 20:38:49 +0900

    fix: import setting

--- TravelerProject-backend/src/main/java/com/traveler/app/config/SecurityConfig.java
+++ TravelerProject-backend/src/main/java/com/traveler/app/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.traveler.app.config;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
